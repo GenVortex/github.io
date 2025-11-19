fa03e0fb149ab5d51bd9feb4e60ef44b848d4e4b
GenVortex <jeanmichelw@posteo.net>
2025-11-19 20:19:00 +0100

    Delete force-newtab.js

--- assets/js/force-newtab.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Force TOUS les liens à s'ouvrir dans un nouvel onglet
-  document.querySelectorAll("a[href]").forEach(function(a) {
-    a.target = "_blank";
-    a.rel = "noopener noreferrer";
-  });
-});
